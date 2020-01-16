37d2510744aa963d741183816c68166a936edd6b
szymonklo <58526555+szymonklo@users.noreply.github.com>
2020-01-16 20:10:53 +0100

    Average + write to file

--- File/Point.cs
+++ File/Point.cs
@@ -10,5 +10,42 @@ namespace File
         public double Lat { get; set; }
         public double Lon { get; set; }
         public double Ele { get; set; }
+        public double AveEle { get; set; }
+
+        public double Asc { get; set; }
+        public double AveAsc { get; set; }
+
+        public double Section { get; set; }
+
+        const double PIx = 3.141592653589793;
+        const double RADIUS = 6378.16;
+
+        /// <summary>
+        /// Convert degrees to Radians
+        /// </summary>
+        /// <param name="x">Degrees</param>
+        /// <returns>The equivalent in radians</returns>
+        public static double Radians(double x)
+        {
+            return x * PIx / 180;
+        }
+
+        /// <summary>
+        /// Calculate the distance between two places.
+        /// </summary>
+        /// <param name="lon1"></param>
+        /// <param name="lat1"></param>
+        /// <param name="lon2"></param>
+        /// <param name="lat2"></param>
+        /// <returns></returns>
+        public static double Distance(Point p1, Point p2)
+        {
+            double dlon = Radians(p2.Lon - p1.Lon);
+            double dlat = Radians(p2.Lat - p1.Lat);
+
+            double a = (Math.Sin(dlat / 2) * Math.Sin(dlat / 2)) + Math.Cos(Radians(p1.Lat)) * Math.Cos(Radians(p2.Lat)) * (Math.Sin(dlon / 2) * Math.Sin(dlon / 2));
+            double angle = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            return angle * RADIUS;
+        }
     }
 }
--- File/Program.cs
+++ File/Program.cs
@@ -29,12 +29,13 @@ namespace File
             string f = fileContent;
             sr.Close();
 
+            DateTime start = DateTime.Now;
+
             int j = 0;
 
             var pointList = new List<Point>();
             var l = pointList;
 
-            int counter = 0;
             string type = "";
 
             for (int i = 0; i<f.Length; i++)
@@ -46,8 +47,15 @@ namespace File
                     {
                         ele += f[i];
                     }
-                    Console.WriteLine(ele);
-                    Console.ReadKey();
+                    //Console.WriteLine(ele);
+                    l[j].Ele = Double.Parse(ele, System.Globalization.CultureInfo.InvariantCulture);
+                    l[j].Ind = j;
+                    j++;
+                    //if (j == 10)
+                      //  break;
+                    //if (j==3)
+                    //    Console.WriteLine(j);
+                    //Console.ReadKey();
                 }
                 if (f[i]=='<')
                 {
@@ -72,7 +80,7 @@ namespace File
                                             {
                                                 lat += f[i];
                                             }
-                                            Console.WriteLine(lat);
+                                            //Console.WriteLine(lat);
                                             break;
                                         }
                                         if (subtype == "lon=")
@@ -81,7 +89,7 @@ namespace File
                                             {
                                                 lon += f[i];
                                             }
-                                            Console.WriteLine(lon);
+                                            //Console.WriteLine(lon);
                                             break;
                                         }
                                         else
@@ -94,7 +102,9 @@ namespace File
                             i--;
                             l.Add(new Point());
                             //l[j].Lat = Convert.ToDouble(lat);
-                            //l[j].Lat = Double.Parse(lat, System.Globalization.CultureInfo.InvariantCulture);
+                            l[j].Lat = Double.Parse(lat, System.Globalization.CultureInfo.InvariantCulture);
+                            l[j].Lon = Double.Parse(lon, System.Globalization.CultureInfo.InvariantCulture);
+
                             //Console.WriteLine(l[j].Lat);
                             //i++;
 
@@ -107,11 +117,80 @@ namespace File
                 }
             }
 
+            TimeSpan dur = start - DateTime.Now;
+            Console.WriteLine($"Reading: {dur}");
+
+            double distanceOfAverage = 0.1;
+            double sumAsc = 0;
+            double sumAveAsc = 0;
+
+            start = DateTime.Now;
+            l[0].AveEle = l[1].Ele;
+            for (j = 1;j<l.Count;j++)
+            {
+                l[j].Section = Point.Distance(l[j], l[j - 1]);
+                
+
+                double sumOfEle = 0;
+                int numOfPoints = 0;
+                for (int k=j;k>=0;k--)
+                {
+                    if (Point.Distance(l[j], l[k]) < distanceOfAverage)
+                    {
+                        sumOfEle += l[k].Ele;
+                        numOfPoints++;
+                    }
+                    else break;
+                }
+                for (int k = j+1; k < l.Count; k++)
+                {
+                    if (Point.Distance(l[j], l[k]) < distanceOfAverage)
+                    {
+                        sumOfEle += l[k].Ele;
+                        numOfPoints++;
+                    }
+                    else break;
+                }
+
+                l[j].AveEle = sumOfEle / numOfPoints;
+
+                l[j].Asc = (l[j].Ele - l[j - 1].Ele);
+                sumAsc += (l[j].Asc > 0 ? l[j].Asc : 0);
+
+                l[j].AveAsc = (l[j].AveEle - l[j - 1].AveEle);
+                sumAveAsc += (l[j].AveAsc > 0 ? l[j].AveAsc : 0);
+
+
+            }
+            dur = start - DateTime.Now;
+            Console.WriteLine($"Processing: {dur}");
+            Console.WriteLine($"Asc: {sumAsc}");
+            Console.WriteLine($"AveAsc: {(int)sumAveAsc}");
+
+            start = DateTime.Now;
+
+            string newContent = "<?xml version=\"1.0\" encoding=\"UTF - 8\"?><gpx><trk><trkseg>";
+            foreach (Point p in l)
+            {
+                newContent += "<trkpt";
+                newContent += " lat=\"";
+                newContent += p.Lat.ToString().Replace(',','.');
+                newContent += "\" lon=\"";
+                newContent += p.Lon.ToString().Replace(',', '.');
+                newContent += "\">";
+                newContent += "<ele>";
+                newContent += p.AveEle.ToString().Replace(',', '.');
+                newContent += "</ele></trkpt>";
+            }
+            newContent += "</trkseg></trk></gpx>";
+
             //inne
             //FileStream fs = new FileStream(path+newFileName+ext, FileMode.Create);
             //fs.Close();
             //Console.Write("File has been created and the Path is D:\\csharpfile.txt");
             //Console.ReadKey();
+
+            Console.WriteLine();
 
             FileInfo newFi = new FileInfo(path + newFileName + ext);
 
@@ -120,12 +199,15 @@ namespace File
             //get the StreamWriter
             StreamWriter sw = new StreamWriter(fsToWrite);
             //write some text using StreamWriter
-            sw.WriteLine("Another line from streamwriter");
+            sw.WriteLine(newContent);
             sw.Close();
 
             //close all Stream objects
             fsToRead.Close();
             fsToWrite.Close();
+
+            dur = start - DateTime.Now;
+            Console.WriteLine($"Writing: {dur}");
         }
     }
 }
